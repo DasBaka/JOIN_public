06e0f814d05356ade47144bc089c59262136f1bb
Stefan Kiesel <stefan.kiesel@f24.com>
2023-03-23 10:41:07 +0100

    Add drag and drop with styling

--- scripts/board.js
+++ scripts/board.js
@@ -1,15 +1,39 @@
+let activeDragDropElement;
+
 function initBoard() {
+    renderColumns();
     renderTasks();
 }
 
+function renderColumns() {
+    let boardWrapper = document.getElementById('board-wrapper');
+    boardWrapper.innerHTML = '';
+
+    for (let i = 0; i < statuses.length; i++) {
+        const status = statuses[i];
+        boardWrapper.innerHTML += /*html*/ `
+        <div class="board-column">
+            <div class="board-column-headline">
+                <h3>${status['displayName']}</h3>
+                <button class="board-column-add-task-button"><img src="assets/img/cross.png" alt="cross"></button>
+            </div>
+            <div id="board-tasks-column-${status['name']}" class="board-tasks-wrapper" ondrop="moveElementTo('${status['name']}')"
+                ondragover="allowDrop(event); dragAreaHighlight('start', '${status['name']}')" ondragleave="dragAreaHighlight('leave', '${status['name']}')"></div>
+        </div>
+        `;
+        document.getElementById(`board-tasks-column-${status['name']}`).innerHTML = '';
+    }
+}
+
 function renderTasks() {
+
     for (let i = 0; i < tasks.length; i++) {
         const task = tasks[i];
         let content = document.getElementById(`board-tasks-column-${task['status']}`);
         let categoryIndex = categories.findIndex(key => key.name === task['category']);
 
         content.innerHTML += /*html*/ `
-        <div class="task-preview-wrapper">
+        <div draggable="true" ondragstart="startDraggingElement(${task['id']})" class="task-preview-wrapper">
             <div style="background-color: ${categories[categoryIndex]['color']};" 
                 class="board-task-category">${task['category']}</div>
             <h5>${task['title']}</h5>
@@ -29,5 +53,31 @@ function renderTasks() {
                 class="board-user-icon">${assigneeInitials}</div>
             `;
         }
+    }
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function moveElementTo(status) {
+    tasks[activeDragDropElement]['status'] = status;
+    initBoard();
+}
+
+function startDraggingElement(id) {
+    activeDragDropElement = id;
+}
+
+function dragAreaHighlight(action, area) {
+    for (let i = 0; i < statuses.length; i++) {
+        const status = statuses[i];
+        document.getElementById('board-tasks-column-' + status['name']).classList.add('dragAreaHighlight');
+    }
+
+    if (action == 'start') {
+        document.getElementById('board-tasks-column-' + area).classList.add('activeDragAreaHighlight');
+    } else if (action == 'leave') {
+        document.getElementById('board-tasks-column-' + area).classList.remove('activeDragAreaHighlight');
     }
 }
--- scripts/data.js
+++ scripts/data.js
@@ -52,21 +52,105 @@ let users = [
 
 let tasks = [
     {
-        "id": 1,
-        "title": "Task 1",
-        "description": "Description 1",
-        "category": "Sales",
+        "id": 0,
+        "title": "Website redesign",
+        "description": "Modify the contents of the main website...",
+        "category": "Design",
         "assignees": [ "anton.meyer", "anja.schulz", "benedikt.ziegler" ],
         "due_date": "2023-04-01",
-        "priority": "Medium",
+        "priority": "low",
         "status": "to-do",
         "subtasks": [
             {
-                "id": 1,
+                "id": 0,
                 "title": "Subtask 1",
+                "Status": "open"
+            },
+            {
+                "id": 1,
+                "title": "Subtask 2",
                 "Status": "open"
             }
         ]
+    },
+    {
+        "id": 1,
+        "title": "Call Potential",
+        "description": "Make the product presentation to prospective buyers",
+        "category": "Sales",
+        "assignees": [ "anton.meyer", "anja.schulz", "benedikt.ziegler" ],
+        "due_date": "2023-04-01",
+        "priority": "high",
+        "status": "in-progress",
+        "subtasks": []
+    },
+    {
+        "id": 2,
+        "title": "Accounting invoices",
+        "description": "Write open invoices for customer",
+        "category": "Backoffice",
+        "assignees": [ "anton.meyer", "anja.schulz", "benedikt.ziegler" ],
+        "due_date": "2023-04-01",
+        "priority": "medium",
+        "status": "awaiting-feedback",
+        "subtasks": []
+    },
+    {
+        "id": 3,
+        "title": "Video cut",
+        "description": "Edit the new company video",
+        "category": "Media",
+        "assignees": [ "anton.meyer", "anja.schulz", "benedikt.ziegler" ],
+        "due_date": "2023-04-01",
+        "priority": "medium",
+        "status": "awaiting-feedback",
+        "subtasks": []
+    },
+    {
+        "id": 4,
+        "title": "Social media strategy",
+        "description": "Develop an ad campaign for brand positioning",
+        "category": "Marketing",
+        "assignees": [ "anton.meyer", "anja.schulz", "benedikt.ziegler" ],
+        "due_date": "2023-04-01",
+        "priority": "low",
+        "status": "done",
+        "subtasks": [
+            {
+                "id": 0,
+                "title": "Subtask 1",
+                "Status": "open"
+            },
+            {
+                "id": 1,
+                "title": "Subtask 2",
+                "Status": "open"
+            },
+            {
+                "id": 2,
+                "title": "Subtask 3",
+                "Status": "open"
+            }
+        ]
+    }
+]
+
+let statuses = [
+    {
+        "name": "to-do",
+        "displayName": "To Do"
+    },
+    {
+        "name": "in-progress",
+        "displayName": "In Progress"
+    },
+    {
+        "name": "awaiting-feedback",
+        "displayName": "Awaiting Feedback"
+    },
+    {
+        "name": "done",
+        "displayName": "Done"
     }
 ]
 
