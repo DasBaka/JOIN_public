620af3ab59de38cfbba546ccfe72ee89c0f0bf25
DasBaka <thebakalestear@gmail.com>
2023-03-23 11:29:07 +0100

    MK first commit

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,20 +1,137 @@
-function renderContacts() {
-    let content = document.getElementById('contact-list-wrapper');
-    content.innerHTML = '';
+let users = [
+  {
+    name: 'Anton Mayer',
+    mail: 'antom@gmail.com',
+    color: 'orange',
+  },
+  {
+    name: 'Anja Schulz',
+    mail: 'schulz@hotmail.com',
+    color: 'violet',
+  },
+  {
+    name: 'Benedikt Ziegler',
+    mail: 'benedikt@gmail.com',
+    color: 'lightgreen',
+  },
+  {
+    name: 'David Eisenberg',
+    mail: 'davidberg@gmail.com',
+    color: 'red',
+  },
+  {
+    name: 'Eva Fischer',
+    mail: 'eva@gmail.com',
+    color: 'lightblue',
+  },
+  {
+    name: 'Emmanuel Maurer',
+    mail: 'emmanuelma@gmail.com',
+    color: 'lightgrey',
+  },
+  {
+    name: 'Marcel Bauer',
+    mail: 'bauer@gmail.com',
+    color: 'lightpink',
+  },
 
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-        
-        content.innerHTML += /*html*/ `
-        <div>
-            <div id="user-icon-${i}" class="user-icon">${user.firstname.charAt(0)}${user.lastname.charAt(0)}</div>
-            <div>
-                <h4>${user.firstname} ${user.lastname}</h4>
-                <h6>${user.mail}</h6>
-            </div>
-        </div>
-        `;
+  {
+    name: 'Jonas Holl',
+    mail: 'jh1234@gmail.com',
+    color: 'lightblue',
+  },
 
-        document.getElementById(`user-icon-${i}`).style.backgroundColor = user.color;
+  {
+    name: 'Johannes Baum',
+    mail: 'treethree@gmail.com',
+    color: 'yellow',
+  },
+  {
+    name: 'Friedrich Mai',
+    mail: 'f82mai@gmail.com',
+    color: 'blue',
+  },
+  {
+    name: 'Stefanie Sauer',
+    mail: 'sauermachtlustig@gmail.com',
+    color: 'red',
+  },
+];
+
+let groupedUsers;
+
+function prepareContacts() {
+  sortContacts();
+  addGroup();
+  groupedUsers = Object.values(groupItems(users, 'letter'));
+}
+
+function sortContacts() {
+  users.sort(function (a, b) {
+    if (a.name.toUpperCase() < b.name.toUpperCase()) {
+      return -1;
     }
+    if (a.name.toUpperCase() > b.name.toUpperCase()) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+function initialLetter(arrObj, pos) {
+  return arrObj.name.charAt(pos);
+}
+
+function addGroup() {
+  users.forEach((element) => {
+    element['letter'] = initialLetter(element, 0);
+  });
+}
+
+function initContainer(id) {
+  let container = document.getElementById(id);
+  container.innerHTML = '';
+}
+
+function addIntoContainer(id, content) {
+  let container = document.getElementById(id);
+  container.innerHTML += content;
+}
+
+function renderContacts() {
+  initContainer('contact-list-wrapper');
+  for (let i = 0; i < groupedUsers.length; i++) {
+    addIntoContainer('contact-list-wrapper', `<h4 class="contact-list-divider">${groupedUsers[i]['group']}</h4>`);
+    let contactArray = groupedUsers[i]['value'];
+    for (let j = 0; j < contactArray.length; j++) {
+      addIntoContainer('contact-list-wrapper', contactCardTemplate(contactArray[j], i, j));
+      document.getElementById(`user-icon-${i}-${j}`).style.backgroundColor = contactArray[j].color;
+    }
+  }
+}
+
+function contactCardTemplate(user, i, j) {
+  return /*html*/ `
+  <input type="radio" name="contact-list-btns" id="contact-btn-${i}-${j}" class="contact-btn">
+  <label for="contact-btn-${i}-${j}" class="contact-wrapper">
+      <div id="user-icon-${i}-${j}" class="user-icon">${initialLetter(user, 0)}${initialLetter(user, user.name.search(' ') + 1)}</div>
+      <div class="contact-details-short">
+          <h4>${user.name}</h4>
+          <h6 style="color: #007cee">${user.mail}</h6>
+      </div>
+  </div>
+
+  `;
+}
+
+function groupItems(objArray, key) {
+  return objArray.reduce((acc, element) => {
+    let group = element[key];
+    if (!acc[group]) {
+      acc[group] = { group, value: [element] };
+    } else {
+      acc[group].value.push(element);
+    }
+    return acc;
+  }, {});
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,92 +1,47 @@
-let users = [
-    {
-        "firstname": "Anton",
-        "lastname": "Mayer",
-        "mail": "antom@gmail.com",
-        "color": "orange"
-    },
-    {
-        "firstname": "Anja",
-        "lastname": "Schulz",
-        "mail": "schulz@hotmail.com",
-        "color": "violet"
-    },
-    {
-        "firstname": "Benedikt",
-        "lastname": "Ziegler",
-        "mail": "benedikt@gmail.com",
-        "color": "lightgreen"
-    },
-    {
-        "firstname": "David",
-        "lastname": "Eisenberg",
-        "mail": "davidberg@gmail.com",
-        "color": "red"
-    },
-    {
-        "firstname": "Eva",
-        "lastname": "Fischer",
-        "mail": "eva@gmail.com",
-        "color": "lightblue"
-    },
-    {
-        "firstname": "Emmanuel",
-        "lastname": "Mauer",
-        "mail": "emmanuelma@gmail.com",
-        "color": "lightgrey"
-    },
-    {
-        "firstname": "Marcel",
-        "lastname": "Bauer",
-        "mail": "bauer@gmail.com",
-        "color": "lightpink"
-    }
-];
-
 let login_status = false;
 
 async function initialPageLoad() {
-    await includeHTML();
-    activeNavElement();
+  await includeHTML();
+  activeNavElement();
 }
 
 function initialRedirect() {
-    if (login_status == false) {
-        window.location.href = '/log-in.html';
-    } else {
-        window.location.href = '/summary.html';
-    }
+  if (login_status == false) {
+    window.location.href = '/log-in.html';
+  } else {
+    window.location.href = '/summary.html';
+  }
 }
 
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+  let includeElements = document.querySelectorAll('[include-html]');
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute('include-html'); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = 'Page not found';
     }
+  }
 }
 
 function activeNavElement() {
-    let navElements = ['summary', 'board', 'add-task', 'contacts', 'legal-notice'];
-    for (let i = 0; i < navElements.length; i++) {
-        const element = navElements[i];
-        if (window.location.pathname.includes(element)) {
-            document.getElementById("nav-element-" + element).classList.add('nav-element-active');
-        }
+  let navElements = ['summary', 'board', 'add-task', 'contacts', 'legal-notice'];
+  for (let i = 0; i < navElements.length; i++) {
+    const element = navElements[i];
+    if (window.location.pathname.includes(element)) {
+      document.getElementById('nav-element-' + element).classList.add('nav-element-active');
     }
+  }
 }
 
 function logOutButtonVisibility() {
-    let element = document.getElementById('log-out-button').classList;
-    if (element.contains('display-none')) {
-        element.remove('display-none');
-    } else {
-        element.add('display-none');
-    }
+  let element = document.getElementById('log-out-button').classList;
+  if (element.contains('display-none')) {
+    element.remove('display-none');
+  } else {
+    element.add('display-none');
+  }
 }
